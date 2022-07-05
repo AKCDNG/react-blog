6624a2fe7fe0ad427b2715169f7a91cbc3b60676
Anish <anishkakaiya@outlook.com>
2022-07-05 11:21:11 +0100

    add dynamic values

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog'
+  const likes = 50
+  const link = 'http://www.google.co.uk'
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        <p>{ Math.random() }</p>
+
+        <a href={ link }>Google Site</a>
       </div>
     </div>
   );
 }
 
 export default App;
+ 
