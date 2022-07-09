e1edefe6c5275c549c6d9dc3b8c66f0711b77643
Anish <anishkakaiya@outlook.com>
2022-07-09 18:46:36 +0100

    lesson 13

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -3,15 +3,20 @@ import BlogList from './blogList';
 
 const Home = () => {
 
-  const blogs = [
+  const [blogs, setBlogs] = useState ([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ]; 
+  ]); 
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id != id)
+    setBlogs(newBlogs)
+  }
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="New Blogs" />
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs"/>
+      <BlogList blogs={blogs} title="New Blogs" handleDelete={handleDelete} />
     </div>
    );
 }
