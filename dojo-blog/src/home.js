b6acde8429dfae6ee7c50a067d4de485e7ad87c4
Anish <anishkakaiya@outlook.com>
2022-07-09 20:30:21 +0100

    lesson 18

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -2,10 +2,8 @@ import { useState, useEffect} from 'react';
 import BlogList from './blogList';
 
 const Home = () => {
-
-  const [name, setName] = useState('Mario');
-
   const [blogs, setBlogs] = useState (null); 
+  const [isPending, setIsPending] = useState(true)
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
@@ -13,13 +11,14 @@ const Home = () => {
         return res.json()
       })
       .then((data) => {
-        console.log(data)
         setBlogs(data)
+        setIsPending(false)
       })
   }, [])
 
   return (
     <div className="home">
+      { isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="New Blogs" />}
     </div>
    );
