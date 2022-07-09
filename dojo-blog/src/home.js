39dabd1f61cae6c680ad8bb5e2103d9d8c054638
Anish <anishkakaiya@outlook.com>
2022-07-09 20:16:56 +0100

    lesson 17

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -5,26 +5,22 @@ const Home = () => {
 
   const [name, setName] = useState('Mario');
 
-  const [blogs, setBlogs] = useState ([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ]); 
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id)
-    setBlogs(newBlogs)
-  }
+  const [blogs, setBlogs] = useState (null); 
 
   useEffect(() => {
-    console.log("Use Effect Ran")
-  }, [name])
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json()
+      })
+      .then((data) => {
+        console.log(data)
+        setBlogs(data)
+      })
+  }, [])
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="New Blogs" handleDelete={handleDelete} />
-      <p>{ name }</p>
-      <button onClick={() => setName('Luigi') }>click me</button>
+      {blogs && <BlogList blogs={blogs} title="New Blogs" />}
     </div>
    );
 }
